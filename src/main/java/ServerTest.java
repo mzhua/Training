08c3e78d5e93cf7181f55ae7ea1f0875237984de
hua <mzhua78@hotmail.com>
2016-08-30 21:35:52 +0800

    first commit

---
+++ src/main/java/ServerTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by john on 16/8/18.
+ */
+
+public class ServerTest {
+    @Test
+    public void x() throws Exception{
+        Server.main(new String[]{"a","b"});
+    }
+}
